5d5768ae0a24086a9a14044a2e98b6240579222a
jams2 <public@elysee-munn.family>
2025-08-14 14:50:05 +0100

    Initial commit

---
+++ tutorial/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-selh1naf-n-#ao%4doiwvh2+9^8ma1rtrb7ghgem+%qj4g5c58"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
